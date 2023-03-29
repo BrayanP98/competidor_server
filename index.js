2d7945ca87a7ddeaaf3ec8cfce86bcbb016bc6ec
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-03-29 10:56:16 -0500

    add database file

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ require('dotenv').config()
 var bodyParser = require('body-parser');
 require('./database');
 var app = express();
+const path= require('path');
 const config= require('./config')
 
 // Convierte una petición recibida (POST-GET...) a objeto JSON
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || config.PORT);
+app.use(express.static(path.join(__dirname,"public")));
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req, res){
-	res.send('holi')
+	res.render('index.ejs')
 });
 
 app.listen(config.PORT, ()=>{
